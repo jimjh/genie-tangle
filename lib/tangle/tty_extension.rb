b21a5eee21995f40c0f0ab62fae1066ead4ef047
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-06-19 14:48:05 -0400

    [#51971617] rejects input for missing terminal

--- lib/tangle/tty_extension.rb
+++ lib/tangle/tty_extension.rb
@@ -5,15 +5,14 @@ module Tangle
   class TTY
 
     # Faye Extension that handles TTY events.
-    # @todo TODO on invalid input, send kill (or some error message)
     # @todo TODO security
     class Extension
 
       def incoming(message, callback)
-        imessage = message.with_indifferent_access
-        case imessage[:channel]
-        when %r[\A/\d+/input\z] then handle_input  imessage
-        when %r[\A/\d+/kill\z]  then handle_kill   imessage
+        message = message.with_indifferent_access
+        case message[:channel]
+        when %r[\A/\d+/input\z] then handle_input  message
+        when %r[\A/\d+/kill\z]  then handle_kill   message
         end
       ensure
         callback.call(message)
@@ -24,9 +23,13 @@ module Tangle
       # Finds appropriate terminal and forwards input to it.
       def handle_input(message)
         user    = extract_user message
+        id      = message[:data][:id]
         request = message[:data][:args]
-        return unless (term = TTY[user, request.shift])
-        term.write << request.join
+        if (term = TTY[user, id])
+          term.write << request.join
+        else
+          message[:error] = 'Terminal does not exist.'
+        end
       end
 
       # Finds appropriate terminal and kills it.
